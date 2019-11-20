2c62417e89f6415cbd1d0a68c1aacd22327bc180
Anick Saha <anicksaha05@gmail.com>
2019-11-19 19:33:10 -0600

    ...

--- utils_regions.py
+++ utils_regions.py
@@ -1,6 +1,7 @@
 ## This file contains all the necessary functions 
 ## to generate regions from variabilities
 
+import numpy
 def get_regions_info(variabilities):
     increase_windows = get_windows(variabilities, 20, True)
     decrease_windows = get_windows(variabilities, 20, False)
@@ -13,25 +14,68 @@ def get_score(seq):
     for i in range(1,len(seq)):
         res += seq[i] - seq[i - 1]
     return res
+
+def get_windows(variabilities, num_regions, increasing = True):
+    n = len(variabilities)
+    windows = []
+    vis = [0 for i in range(n)]
+    X = []
+    Y = []
+    processed = 0
     
+    for window_size in range(20, 31):
+        for i in range(n - window_size + 1):
+            end = i + window_size - 1
+            curr = []
+            for j in range(i, end + 1):
+                curr.append(variabilities[j])
+            
+            score = get_score(curr)
+            windows.append([score, [i, window_size]])
+
+    windows = sorted(windows, reverse = increasing)
+    
+    for segment in windows:
+        start = segment[1][0]
+        end = segment[1][0] + segment[1][1] - 1
+        valid = 1
+        
+        for i in range(start, end + 1):
+            if vis[i] == 1:
+                valid = 0
+                break
+
+        if valid == 1:
+            x = []
+            y = []
+            for i in range(start, end + 1):
+                vis[i] = 1
+                x.append(i)
+                y.append(variabilities[i])
+            processed += 1
+            X.append(x)
+            Y.append(y)
+            
+        if processed == num_regions:
+            return [X, Y]
+
 def merge_windows(increase_windows, decrease_windows, num_regions):
     regions_info = []
     vis = [0 for i in range(len(increase_windows[0]))]
-    done = 0
+    processed = 0
     
-    for seq1 in range(len(decrease_windows[0])):
-        start_val1 = decrease_windows[0][seq1][0]
+    for sequence_1 in range(len(decrease_windows[0])):
+        start_val1 = decrease_windows[0][sequence_1][0]
         minVal = 1e9
         idx1 = -1
         idx2 = -1
-        L1 = []
-        L2 = []
-        for seq2 in range(len(increase_windows[0])):
-            end_val1 = increase_windows[0][seq2][-1]
-            if vis[seq2] == 0 and end_val1 - start_val1 + 1 < minVal and end_val1 >= start_val1:
+        L1 = L2 = []
+        for sequence_2 in range(len(increase_windows[0])):
+            end_val1 = increase_windows[0][sequence_2][-1]
+            if vis[sequence_2] == 0 and end_val1 - start_val1 + 1 < minVal and end_val1 >= start_val1:
                 minVal = end_val1 - start_val1 + 1
-                idx1 = seq1
-                idx2 = seq2
+                idx1 = sequence_1
+                idx2 = sequence_2
                 start = start_val1
                 end = end_val1
         if (idx2 == -1):
@@ -39,54 +83,10 @@ def merge_windows(increase_windows, decrease_windows, num_regions):
 
         regions_info.append([start, end])
         vis[idx2] = 1
-        done += 1
+        processed += 1
 
-        if(done == num_regions):
+        if(processed == num_regions):
             break
 
     regions_info = sorted(regions_info)
     return regions_info
-
-def get_windows(Y, num_regions, increasing = True):
-    n = len(Y)
-    windows = []
-    vis = [0 for i in range(n)]
-    retX = []
-    retY = []
-    ret = 0
-    
-    # Limits the window size of a decreasing region to be between 20 and 30.
-    for window_size in range(20, 31):
-        for i in range(n - window_size + 1):
-            end = i + window_size - 1
-            prev = []
-            for j in range(i, end + 1):
-                val_at_j = 0
-                prev.append(Y[j])
-            
-            score = get_score(prev)
-            windows.append([score, [i, window_size]])
-
-    windows = sorted(windows, reverse=increasing)
-    
-    for L in windows:
-        start = L[1][0]
-        end = L[1][0] + L[1][1] - 1
-        flag = 0
-        for i in range(start, end + 1):
-            if vis[i] == 1:
-                flag = 1
-                break
-        if flag == 0:
-            x = []
-            y = []
-            for i in range(start, end + 1):
-                vis[i] = 1
-                x.append(i)
-                y.append(Y[i])
-            ret += 1
-            retX.append(x)
-            retY.append(y)
-            
-        if ret == num_regions:
-            return [retX, retY]
